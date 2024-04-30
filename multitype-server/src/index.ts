11119b87736a871ab82706f41564c9557a7be4c6
drshnn <darshansu156@gmail.com>
2024-04-30 22:09:56 +0530

    created socket.io server

--- multitype-server/src/index.ts
+++ multitype-server/src/index.ts
@@ -1,7 +1,26 @@
+
 import { log } from 'console';
 import express from 'express';
+import http from 'http'
+import { Server } from 'socket.io';
 
-const app = express();
-app.listen(3000, () => {
-    log("server started 3000")
+
+const app = express()
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+
+
+io.on('connect', (socket) => {
+    log('user connected with id: ', socket.id.toString())
+    // socket.join(randomUUID())
+    // //list rooms
+    // log(io.sockets.adapter.rooms)
+})
+
+server.listen(3000, () => {
+    log('server runnin on 3000')
 })
