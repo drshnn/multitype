a7eb9cb080c4543728c0d3e792ca7df58ca75717
Darshan Suthar <darshansu156@gmail.com>
2024-05-06 08:03:01 +0530

    refectored code

--- multitype-server/src/index.ts
+++ multitype-server/src/index.ts
@@ -1,32 +1,40 @@
+import { log } from "console";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import Room from "./utils/Room";
 
-import { log } from 'console';
-import express from 'express';
-import http from 'http'
-import { Server } from 'socket.io';
-import Room from './utils/Room';
-
-
-const app = express()
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173"
-    }
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
 
-
-io.on('connect', (socket) => {
-    const username = socket.handshake.query['username']?.toString()
-    log('user connected with id: ', socket.id.toString(), "username", socket.handshake.query['username']?.toString())
-    const room = Room.joinEmptyRoom(username ?? '', socket)
-    socket.emit('joinedRoom', { roomId: room.roomId, users: room.userNames, words: room.words })
-    socket.on('disconnect', () => {
-        log("user disconnected with id ", socket.id.toString())
-        room.leaveRoom(socket.id.toString())
-        log(room.getRoomSize())
-    })
-})
+io.on("connect", (socket) => {
+  const username = socket.handshake.query["username"]?.toString();
+  log(
+    "user connected with id: ",
+    socket.id.toString(),
+    "username",
+    socket.handshake.query["username"]?.toString()
+  );
+  const room = Room.joinEmptyRoom(username ?? "", socket);
+  socket.emit("joinedRoom", {
+    roomId: room.roomId,
+    users: room.userNames,
+    words: room.words,
+  });
+  io.to(room.roomId).emit("newUserJoined", { users: room.userNames });
+  socket.on("disconnect", () => {
+    log("user disconnected with id ", socket.id.toString());
+    room.leaveRoom(socket.id.toString());
+    log(room.getRoomSize());
+    io.to(room.roomId).emit("someUserLeft", { users: room.userNames });
+  });
+});
 
 server.listen(3000, () => {
-    log('server runnin on 3000')
-})
+  log("server runnin on 3000");
+});
